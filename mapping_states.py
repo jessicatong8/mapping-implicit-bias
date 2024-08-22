80443aa697a1eecf1c38135153f696602cf4c676
jessicat8 <jessicatong14@gmail.com>
2024-08-21 22:11:42 -0700

    state map using geojson

--- mapping_states.py
+++ mapping_states.py
@@ -1,31 +1,28 @@
 import pandas as pd
 import plotly.express as px
 from jinja2 import Template
+import json
  
 #import data
 data = pd.read_csv('2020_RaceAverageScore_Cleaned.csv')
 
-# create choropleth map for the data
-# color will be the column to be color-coded
-# locations is the column with sppatial coordinates
-fig = px.choropleth(data, 
-                    locations='State' ,
-                    locationmode="USA-states", 
-                    color='Average Score', 
+with open('us-states.json') as response:
+     states = json.load(response)
+
+
+fig = px.choropleth(data, geojson=states, locations='State', color='Average Score',
+                    range_color=(0.2,0.3),
+                    color_continuous_scale="Darkmint",
                     scope="usa",
-                    range_color=(data['Average Score'].min(), data['Average Score'].max()),
-                    color_continuous_scale="Blues",
-
                     )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_geos(showlakes=False)
 
-fig.update_layout(
-    geo = dict(showlakes=False),
-    autosize = False,
-    margin=dict(l=0, r=0, t=0, b=0),
-    #height = 300
-    )
- 
+# fig.update_traces(marker_line_color='gray',  # Border color
+#                   marker_line_width=0.5)        # Border width
+
 fig.show()
+
 
 outputFile = r"index.html"
 templateFile = r"template.html"
