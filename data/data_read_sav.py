bdc6f429aa958a16e04b81bf8252737a32736d4e
jessicat8 <jessicatong14@gmail.com>
2025-01-09 19:57:02 -0800

    separate county operations, round

--- data/data_read_sav.py
+++ data/data_read_sav.py
@@ -15,7 +15,7 @@ state_fips_dtype = {
 state_fips = pd.read_csv('mapping-implicit-bias/data/us-state-fips.csv', dtype=state_fips_dtype)
 
 
-def cleanChunk(chunk):
+def cleanChunk(chunk, group):
     # filter for country of residence, state, and overall IAT D score
     # PCT_error_3467 = % of error in combined-task blocks
     # pct_300 = % of latencies below 300 ms
@@ -36,48 +36,61 @@ def cleanChunk(chunk):
             # exclude reaction times <300 ms on >10% of trials
             (df["percentRT<300"] <= 10)]
     
-    # cast to correct data types
-    df['year'] = df['year'].astype(int)
+    if group == 'county':
+        # add column with state name and number
+        df['stateNo'] = df['stateAbb'].map(state_fips.set_index('stateAbb')['stateNo'])
+        # add column with fips (stateNo + countyNo)
+        df['fips'] = df['stateNo'] + df['countyNo'].astype(str).str.zfill(3)
+        df.drop(['stateNo'], axis=1, inplace=True)
 
-    # add column with state name and number
-    df['stateName'] = df['stateAbb'].map(state_fips.set_index('stateAbb')['stateName'])
-    df['stateNo'] = df['stateAbb'].map(state_fips.set_index('stateAbb')['stateNo'])
-    # add column with fips (stateNo + countyNo)
-    df['fips'] = df['stateNo'] + df['countyNo'].astype(str).str.zfill(3)
+    # round score to 2 decimal places
+    df['score'] = df['score'].round(2)
 
     # drop uneccessary columns from dataframe
-    df.drop(['country', 'countyNo', 'stateNo', 'percentError', 'percentRT<300'], axis=1, inplace=True)
+    df.drop(['country', 'countyNo', 'percentError', 'percentRT<300'], axis=1, inplace=True)
     
+    # print(df.head(5))
     return df
 
 
-def processChunks(file,group):
-    if group == "state":
-        group = "stateAbb"
-    elif group == "county":
-        group = "fips"
-    else:
+def processChunks(file,group,year):
+    # check if group is either state or county
+    if group != "state" and group != "county":
         raise ValueError("Invalid group. Please choose either state or county.")
         
     result = [] 
+    
     # Read spss .sav file in chunks
     reader = pyreadstat.read_file_in_chunks(pyreadstat.read_sav, file, chunksize= 10000)
+    
     for df, meta in reader:
-        cleaned_chunk = cleanChunk(df)
+        cleaned_chunk = cleanChunk(df, group)
         # print(cleaned_chunk.head(10))  # print first 10 rows for testing)
-        if group == 'county':
-            pass
-        result.append(cleaned_chunk)
+        result.append(cleaned_chunk)  
     combined_df = pd.concat(result)
+
     # group by given group, select score column, sums scores and counts number of people
-    grouped = combined_df.groupby(group)['score'].agg(['sum', 'count']).reset_index()
+    if group == "state":
+        key = "stateAbb"
+    elif group == "county":
+        key = "fips"
+    
+    grouped = combined_df.groupby(key)['score'].agg(['sum', 'count']).reset_index()
     # calculate new column with  average score
-    grouped['avgScore'] = (grouped['sum'] / grouped['count'])
+    grouped['avgScore'] = (grouped['sum'] / grouped['count']).round(2)
+    grouped.drop(['sum'], axis=1, inplace=True)
+    
+    if group == "state":
+        grouped['stateName'] = grouped['stateAbb'].map(state_fips.set_index('stateAbb')['stateName'])
+    
+    # add columns with year
+    grouped['year'] = year
+
     return grouped
 
 def main():
-    df = processChunks(file,'state')
-    df.to_csv('mapping-implicit-bias/data/2023_state_faster.csv', index=False)
+    df = processChunks(file,"state",2023)
+    df.to_csv('mapping-implicit-bias/data/2023_state.csv', index=False)
     print(df.to_string())
     print("Process finished --- %s seconds ---" % (time.time() - start_time))
 
