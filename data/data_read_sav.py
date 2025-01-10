295d08396665eeaf9e794d2360450fed4d8ad723
jessicat8 <jessicatong14@gmail.com>
2025-01-09 18:28:09 -0800

    cleaned up a bit, not faster

--- data/data_read_sav.py
+++ data/data_read_sav.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import pyreadstat
+import numpy as np
 import time
 start_time = time.time()
 
-file = "/Users/jessicatong/Documents/IAT/Race_IAT.public.2020.sav"
+file = "/Users/jessicatong/Documents/IAT/RaceIAT.public.2023.sav"
 
 state_fips_dtype = {
     'stateName': 'str',
@@ -18,46 +19,54 @@ def cleanChunk(chunk):
     # filter for country of residence, state, and overall IAT D score
     # PCT_error_3467 = % of error in combined-task blocks
     # pct_300 = % of latencies below 300 ms
-    df = chunk.loc[:,["year","countryres_num", "STATE", "CountyNo", "D_biep.White_Good_all", "PCT_error_3467", "pct_300"]]
+    # df = chunk.loc[:,["year","countryres_num", "STATE", "CountyNo", "D_biep.White_Good_all", "PCT_error_3467", "pct_300"]]
+    df = chunk[["year", "countryres_num", "STATE", "CountyNo", "D_biep.White_Good_all", "PCT_error_3467", "pct_300"]]
     # rename columns
     df.columns = ['year','country', 'stateAbb', 'countyNo','score','percentError','percentRT<300']
     # print(df.head(10))
     
     # filter for inclusion/exclusion critera
-    # exclude missing data
-    df = df.dropna()
-    # include people in the US with state and county info
+    #include people in the US with state and county info
     df = df[(df["country"] == 1.0) &
             (df["stateAbb"] != "") &
             (df["countyNo"] != "") &
+            (df["score"].notna()) &
             # exclude errors on > 30% of trials
             (df["percentError"] <= 30) &
             # exclude reaction times <300 ms on >10% of trials
-            (df["percentRT<300"] <= 10)].copy()
-
+            (df["percentRT<300"] <= 10)]
+    
     # cast to correct data types
     df['year'] = df['year'].astype(int)
-    df['countyNo'] = df['countyNo'].astype(str)
-    df['countyNo'] = df['countyNo'].str.zfill(3)
 
     # add column with state name and number
     df['stateName'] = df['stateAbb'].map(state_fips.set_index('stateAbb')['stateName'])
     df['stateNo'] = df['stateAbb'].map(state_fips.set_index('stateAbb')['stateNo'])
     # add column with fips (stateNo + countyNo)
-    df['fips'] = df['stateNo'] + df['countyNo']
+    df['fips'] = df['stateNo'] + df['countyNo'].astype(str).str.zfill(3)
 
     # drop uneccessary columns from dataframe
     df.drop(['country', 'countyNo', 'stateNo', 'percentError', 'percentRT<300'], axis=1, inplace=True)
+    
     return df
 
 
 def processChunks(file,group):
+    if group == "state":
+        group = "stateAbb"
+    elif group == "county":
+        group = "fips"
+    else:
+        raise ValueError("Invalid group. Please choose either state or county.")
+        
     result = [] 
     # Read spss .sav file in chunks
     reader = pyreadstat.read_file_in_chunks(pyreadstat.read_sav, file, chunksize= 10000)
     for df, meta in reader:
         cleaned_chunk = cleanChunk(df)
         # print(cleaned_chunk.head(10))  # print first 10 rows for testing)
+        if group == 'county':
+            pass
         result.append(cleaned_chunk)
     combined_df = pd.concat(result)
     # group by given group, select score column, sums scores and counts number of people
@@ -67,8 +76,8 @@ def processChunks(file,group):
     return grouped
 
 def main():
-    df = processChunks(file,'stateAbb')
-    df.to_csv('2020_state_sav.csv', index=False)
+    df = processChunks(file,'state')
+    df.to_csv('mapping-implicit-bias/data/2023_state_faster.csv', index=False)
     print(df.to_string())
     print("Process finished --- %s seconds ---" % (time.time() - start_time))
 
