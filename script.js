2fc7fbc520e356f82c82a669cb775464fb121d9e
jessicat8 <jessicatong14@gmail.com>
2025-01-13 22:37:49 -0800

    county map attempt

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ document.getElementById('countyButton').addEventListener('click', function() {
     document.getElementById('stateButton').classList.remove('disabled');
 });
 
+// State Map
 d3.csv('https://raw.githubusercontent.com/jessicatong8/mapping-implicit-bias/main/data/old-csv-data/2020_state_fromcsv.csv').then(function(rows) {
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
     }
-    console.log(rows);
+    // console.log(rows);
 
     var data = [{
         type: 'choropleth',
@@ -44,15 +45,57 @@ d3.csv('https://raw.githubusercontent.com/jessicatong8/mapping-implicit-bias/mai
             scope: 'usa',
             countrycolor: 'rgb(255, 255, 255)',
             showlakes: false,
-
             margin: { t: 0, b: 0, l: 0, r: 0 }
         },
-        // legend: {
-        //     orientation: 'h'
-        // }
     };
 
-    Plotly.newPlot("mapContainer", data, layout, { showLink: false });
+    Plotly.newPlot("stateMap", data, layout, { showLink: false });
 }).catch(function(error) {
     console.error("Error loading CSV data:", error);
+});
+
+// County Map
+d3.json('https://raw.githubusercontent.com/jessicatong8/mapping-implicit-bias/main/geojson/us-counties.json', function(myjson) {
+    d3.csv('https://raw.githubusercontent.com/jessicatong8/mapping-implicit-bias/main/data/old-csv-data/2020_county_fromcsv.csv').then(function(rows) {
+        function unpack(rows, key) {
+            return rows.map(function(row) { return row[key]; });
+        }
+        console.log(rows);
+
+        var data = [{
+            type: 'choropleth',
+            // locationmode: 'geojson-id',
+            locations: unpack(rows, 'fips'),
+            z: unpack(rows, 'avgScore'),
+            // text: unpack(rows, 'State'),
+            colorscale: [
+                [0, '#dfffe2'], [0.2, '#b3e3cf'],
+                [0.4, '#69b8b0'], [0.6, '#3a91a1'],
+                [0.8, '#21728e'], [1, '#123f5a']
+            ],
+            colorbar: {
+                    len:1, 
+                    title: "Average Score", 
+                    titleside:'right',
+                    // ticklabelposition: 'left',
+                    orientation:'v',
+                },
+            geojson: myjson,
+            featureidkey: 'id'
+        }];
+
+        var layout = {
+            title: { text: 'Implicit Racial Bias' },
+            geo: {
+                // scope: 'usa',
+                countrycolor: 'rgb(255, 255, 255)',
+                showlakes: false,
+                margin: { t: 0, b: 0, l: 0, r: 0 }
+            },
+        };
+
+        Plotly.newPlot("countyMap", data, layout, { showLink: false });
+    }).catch(function(error) {
+        console.error("Error loading CSV data:", error);
+    });
 });
